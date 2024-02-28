97e4e4170d8247967369355b67f1bfdf85374904
bengris32 <bengris32@protonmail.ch>
2024-02-28 22:38:02 +0000

    mt6895-common: parts: Import from sm8250-common
    * From LineageOS/android_device_xiaomi_sm8250-common @ 62e8e0551849a1c2d4f1d35a328d5eb70330bf53
    * Drop all features apart from thermal.
    
    Change-Id: I39173f743d0d29dbcc3d88390830ed33570fed56
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6895.mk
+++ mt6895.mk
@@ -195,6 +195,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.recovery.mt6895.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6895.rc
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
