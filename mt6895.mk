63074ba9c5329a136ea30a785dd5d8270a4393c1
ChrisCatto <chriscatto@proton.me>
2024-11-15 16:12:01 +0200

    mt6895-common: Allow targets to enable building super partition
    * This is for the custom fastboot packages we introduced on xaga.
    
    Change-Id: I504b14cd9f509a6ffa58a26808d3833e6de3d12d
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- mt6895.mk
+++ mt6895.mk
@@ -97,7 +97,7 @@ PRODUCT_PACKAGES += \
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_BUILD_SUPER_PARTITION ?= false
 
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl.custom \
