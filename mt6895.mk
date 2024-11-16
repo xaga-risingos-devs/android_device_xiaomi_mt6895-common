3a3c03638b459970bb05d1a845407e5376e0fc4f
ChrisCatto <chriscatto@proton.me>
2024-11-16 10:00:41 +0200

    mt6895-common: Remove unwanted packages
    Change-Id: Ie62fb75628f5584424040d748236c9b233ccb2fc
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- mt6895.mk
+++ mt6895.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/rsc,$(TARGET_COPY_OUT_VENDOR)/etc/rsc)
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6895 \
