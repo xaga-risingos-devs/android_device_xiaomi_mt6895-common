1a60b9d72e36804ee1d0a26f4770f8b04ef45bfc
Yurical <yurical1@outlook.com>
2024-12-22 18:59:21 +0530

    mt6895-common: Do not include other java mini debug info
    In the same manner of PRODUCT_SYSTEM_SERVER_DEBUG_INFO.
    We explicitly add PRODUCT_OTHER_JAVA_DEBUG_INFO here since
    WITH_DEXPREOPT_DEBUG_INFO can be overriden with
    PRODUCT_SYSTEM_SERVER_DEBUG_INFO and PRODUCT_OTHER_JAVA_DEBUG_INFO.
    
    Change-Id: Ib9173bdbeaf3e279256be10c4082036c486dd154
    Signed-off-by: Yurical <yurical1@outlook.com>

--- mt6895.mk
+++ mt6895.mk
@@ -179,6 +179,7 @@ PRODUCT_COPY_FILES += \
 
 # Reduce system server verbosity.
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+PRODUCT_OTHER_JAVA_DEBUG_INFO := false
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
