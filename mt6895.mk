ab3969c4854d4f355b785c61e14b2696ebd578c8
Hecheng Yu <106876912+kde-yyds@users.noreply.github.com>
2025-03-09 22:36:03 +0800

    mt6895: enable 64-bit audio

--- mt6895.mk
+++ mt6895.mk
@@ -45,12 +45,12 @@ PRODUCT_PACKAGES += \
     create_pl_dev.recovery
 
 # Audio
+$(call soong_config_set,android_hardware_audio,run_64bit,true)
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.3-impl \
-    android.hardware.audio.service \
-    com.android.server.audio
+    android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
     audio.primary.default \
