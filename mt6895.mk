ba2e51db4254d599724a6b2337267bd40fb548a1
Hecheng Yu <kde-yyds@qq.com>
2025-04-16 14:01:09 +0000

    mt6895-common: Run 64-bit audio service

--- mt6895.mk
+++ mt6895.mk
@@ -42,6 +42,7 @@ PRODUCT_PACKAGES += \
     create_pl_dev.recovery
 
 # Audio
+$(call soong_config_set,android_hardware_audio,run_64bit,true)
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl:64 \
     android.hardware.audio.effect@7.0-impl:64 \
