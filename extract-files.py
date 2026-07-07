5baa01ffd3ad666cecc7973638407c862b341bec
Hecheng Yu <kde-yyds@qq.com>
2026-07-08 07:17:20 +0800

    mt6895-common: libarmnn_ndk.mtk add needed liblog.so
    * liblog.so was needed by one of the needed libs of libarmnn_ndk.mtk before Android 17, but now not.

--- extract-files.py
+++ extract-files.py
@@ -88,7 +88,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcutils.so'),
     ('vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
         .add_needed('libbase_shim.so'),
-    'vendor/lib64/libteei_daemon_vfs.so': blob_fixup()
+    ('vendor/lib64/libteei_daemon_vfs.so', 'system_ext/lib64/libarmnn_ndk.mtk.so'): blob_fixup()
         .add_needed('liblog.so'),
 }  # fmt: skip
 
